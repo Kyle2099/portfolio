8953fde5cadb935ba59a8a6315a9f6f758b8a511
Kyle Souther-Bruno <ksoutherbruno@gmail.com>
2018-12-18 01:25:10 -0600

    updated php

--- contact.php
+++ contact.php
@@ -1,20 +1,37 @@
 <?php
 
+  $path = '/home/ubuntu/pear/share/pear/';
+  set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+    
+  require_once("Mail.php");
+  $host = "smtp.gmail.com";
+  $port = "587";
+  $username = "ksoutherbruno@gmail.com";
+  $password = "";
+
+  $to = "Kyle Souther-Bruno <ksoutherbruno@gmail.com>";
+  
   if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message'])){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
     $message = nl2br($_POST['message']);
-    $to = "ksoutherbruno@gmail.com";
     $from = $email;
-    $message_format = '<b>Name:</b> '.$name.' <br><b>Email:</b> '.$email.' <p>'.$message.'</p>';
-    $headers = "From: $from\n";
-    $headers .= "MIME-Version: 1.0\n";
-    $headers .= "content-type: text/html; charset=iso-8859-1\n";
-    if(mail($to, $subject, $message_format, $headers)){
-      echo "success";
-    }else{
-      echo "The server failed to send the message. Please try again later.";
+    $headers = array('From' => $from, 'To' => $to, 'Subject' => $subject);
+    $smtp = Mail::factory('smtp', array ('host' => $host,
+                                         'auth' => true,
+                                         'username' => $username,
+                                         'password' => $password));
+    
+    $body = '<b>Name:</b> '.$name.' <br><b>Email:</b> '.$email.' <p>'.$message.'</p>';
+    
+    $mail = $smtp->send($to, $headers, $body);
+    
+    if ( PEAR::isError($mail) ) {
+        echo("The server failed to send the message. Please try again later." . $mail->getMessage() . "</p>");
+    } else {
+        echo("<p>Message sent.</p>");
     }
+    
   }
 ?>
